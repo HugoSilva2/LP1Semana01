7cf6510ba2ae2fd056f5d85edbfc3c7ab450cc2e
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 10:53:15 +0000

    Add XML comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿
+
+using System;
 
 namespace MeuPrograma
 {
+    /// This is a Hello World Program that also says Good Bye
     class Program
     {
+        /// The main part of the code
         static void Main(string[] args)
         {
+            //Console.WriteLine to print something
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Bye World!");
         }

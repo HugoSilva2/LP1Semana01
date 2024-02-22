b94924eea095d14da739f61c491c37f8baf15315
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 15:41:45 +0000

    Add strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase1 = "Damn, what a nice string I am!\n";
+            string frase2 = "Oh! I didn't noticed you there!\n";
+            string frase3 = "HA!\tHA!\tHA!\tHA!";
+            Console.WriteLine(frase1);
+            Console.WriteLine(frase2);
+            Console.WriteLine(frase3);
+        }
+    }
+}

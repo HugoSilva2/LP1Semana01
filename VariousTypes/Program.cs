9e9757e2baab5eefacb6059a5382fbcb9b504a0b
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 11:26:00 +0000

    Add Characters

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,14 +6,23 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
-            //Int Variables
+            //Integer Variables
             int num1 = 420 ;
             int num2 = 0x20 ;
             uint num3 = 0x69U ;
-            
+
             Console.WriteLine(num1);
             Console.WriteLine(num2);
             Console.WriteLine(num3);
+
+            //Character Variables
+            char sharps = '\u1E9E';
+            char omega = '\u03A9';
+            char star = '\u2721';
+
+            Console.WriteLine(sharps);
+            Console.WriteLine(omega);
+            Console.WriteLine(star);
 
         }
     }

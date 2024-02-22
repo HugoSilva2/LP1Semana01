433960c4b08fc60b17b84dfd2b48ea5cb230a699
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 19:00:07 +0000

    Preparing Phase

---
+++ AskForInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input of an integer
+
+            //convert string to integer(saving as a variable)
+
+            //Input of a real number
+
+            //convert string to float(saving as a variable)
+
+            //Addition of both variables 
+
+        }
+    }
+}

e56510b7abd4f95f237d13a7342f0342d4f584a3
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 19:05:21 +0000

    Other input,conversion and final result

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -12,11 +12,12 @@ namespace AskForInput
             //convert string to integer(saving as a variable)
             int num1 = int.Parse(inteiro); 
             //Input of a real number
-
+            Console.WriteLine("Input a real number(ex: decimal):");
+            string numeroreal = Console.ReadLine();
             //convert string to float(saving as a variable)
-
+            float num2 = float.Parse(numeroreal);
             //Addition of both variables 
-
+            Console.WriteLine($"{num1 + num2}");
 
         }
     }

54f25d3ba035340b2062a0ab72067b3d3aa0ffc8
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 11:08:39 +0000

    Add Integers

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Int Variables
+            int num1 = 420 ;
+            int num2 = 0x20 ;
+            uint num3 = 0x69U ;
+            
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+        }
+    }
+}

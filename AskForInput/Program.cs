c0b6580b79932ce84adfa6538dae2e725cb2d6ab
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-02-22 19:04:01 +0000

    First input and conversion

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,14 +7,16 @@ namespace AskForInput
         static void Main(string[] args)
         {
             //Input of an integer
-
+            Console.WriteLine("Input an integer:");
+            string inteiro = Console.ReadLine();
             //convert string to integer(saving as a variable)
-
+            int num1 = int.Parse(inteiro); 
             //Input of a real number
 
             //convert string to float(saving as a variable)
 
             //Addition of both variables 
+
 
         }
     }
